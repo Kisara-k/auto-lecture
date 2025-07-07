4348641a32ccf78112dae89313aeab780770f3b4
Kisara Kodithuwakku <kisarakd@gmail.com>
2025-07-07 09:24:45 +0530

    docs: update example outputs

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ Always maintain a professional, clear, and helpful demeanor. You are here to ass
 user_prompt_1 = """
 Create a detailed, introductory understandable study note based on the following lecture content, make sure it's well organized, ADD CONTENTT AND DETAIL, and is clear and detailed. Explain key concepts clearly and in words. Do not leave anything out that's in the lecture content. Structured liek a note, not just a list of points. Each section must have a in word introduction. Don't use overly academic tone. 
 
-Be VERY DETAILED in each of your explanations. Prefer clear, in-word explanations over brevity. Use emojis in main headings for clarity. Number main headings like 1. [Emoji] Heading. Use between 3 - 12 main headings as needed. Don't use too many sub headings.
+Be VERY DETAILED in each of your explanations. Prefer clear, in-word explanations over brevity. Use emojis in main headings for clarity. Number main headings like 1. [Emoji] Heading. Use between 3 - 10 main headings as needed. Don't use too many sub headings.
 
 lecture content:
 
